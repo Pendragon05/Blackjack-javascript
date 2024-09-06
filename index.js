6e80318657a7a60f0e013381e48a7cf87148b751
James Baranuelo <122170760+Pendragon05@users.noreply.github.com>
2024-09-06 23:00:25 +0900

    Complete getRandom Function

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1; // 0-12 -> 1-13
+  // if 1     -> return 11
+  // if 11-13 -> return 10
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
