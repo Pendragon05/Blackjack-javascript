7a09d5d8043ea0d56d4e9d3ad4855698573e298f
James Baranuelo <122170760+Pendragon05@users.noreply.github.com>
2024-09-07 23:19:08 +0900

    fixed startGame function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
@@ -23,6 +23,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
